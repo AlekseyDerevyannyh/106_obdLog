f94f51154e75ba34af885a888c43cf6e4cd1189d
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2024-11-26 19:19:35 +0300

    add methods readFile & writeFile in Main.java

--- src/main/java/ru/dev/Main.java
+++ src/main/java/ru/dev/Main.java
@@ -1,7 +1,33 @@
 package ru.dev;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    public static List<String> readFile(String fileName) throws IOException {
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName))) {
+            List<String> result = new ArrayList<>();
+            while (fileReader.ready()) {
+                result.add(fileReader.readLine());
+            }
+            return result;
+        }
+    }
+
+    public static void writeFile(String fileName, List<String> lines) throws IOException {
+        try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(fileName))) {
+            for (String line : lines) {
+                fileWriter.write(line);
+            }
+        }
     }
 }
