31ff2ac937a5b334f9d38f828065c09215d5c0f9
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2024-11-26 19:56:10 +0300

    add method monthToNumber to Main.java

--- src/main/java/ru/dev/Main.java
+++ src/main/java/ru/dev/Main.java
@@ -9,8 +9,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws IOException {
+        writeFile("file1", readFile("file"));
     }
 
     public static List<String> readFile(String fileName) throws IOException {
@@ -26,8 +26,26 @@ public class Main {
     public static void writeFile(String fileName, List<String> lines) throws IOException {
         try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(fileName))) {
             for (String line : lines) {
-                fileWriter.write(line);
+                fileWriter.write(line + "\n");
             }
         }
+    }
+
+    public static String monthToNumber(String month) {
+        return switch (month) {
+            case "янв." -> "01";
+            case "февр." -> "02";
+            case "мар." -> "03";
+            case "апр." -> "04";
+            case "мая" -> "05";
+            case "июн." -> "06";
+            case "июл." -> "07";
+            case "авг." -> "08";
+            case "сент." -> "09";
+            case "окт." -> "10";
+            case "нояб." -> "11";
+            case "дек." -> "12";
+            default -> throw new IllegalStateException("Unexpected value: " + month);
+        };
     }
 }
