95fe22202db3a3c73c84ea3ab0e2e7e20030607c
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2025-04-07 21:55:36 +0300

    add class TrackLogConverter

--- src/main/java/ru/dev/Main.java
+++ src/main/java/ru/dev/Main.java
@@ -2,15 +2,14 @@ package ru.dev;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -34,8 +33,6 @@ public class Main {
             inDir = args[0];
             outDir = args[1];
         }
-//        String fileName = "trackLog-2024-10-23_07-22-08.csv";
-
         Set<String> files = getFileNames(inDir);
         if (files == null || files.isEmpty())       return;
         String trackLogRegex = "^trackLog-\\d{4}-.{0,7}-\\d{2}_\\d{2}-\\d{2}-\\d{2}\\.csv$";
@@ -46,9 +43,22 @@ public class Main {
         }
         System.out.println(trackLogFiles);
         System.out.println(Runtime.getRuntime().availableProcessors());
+        File outDirectory = new File(outDir);
+        if (!outDirectory.exists()) {
+            boolean success = outDirectory.mkdirs();
+            if (!success) {
+                System.out.println("Failed to create output directory");
+                return;
+            }
+        }
+        String fileName = "";
+        String outputFileName = outDir + "/" + fileName;
 
+
+
+
+//        Path outPath = Path.of(outDir);
 //        getFileNames(inDir).forEach(System.out::println);
-
 //        String fileName = "trackLog-2024-нояб.-23_07-22-08.csv";
 //        logs = readFile(fileName);
 //        removeHeaderFailLines(logs);
---
+++ src/main/java/ru/dev/TrackLogConverter.java
@@ -0,0 +1,137 @@
+package ru.dev;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class TrackLogConverter implements Runnable {
+    private String fileName;
+    private String inDir;
+    private String outDir;
+    private String header;
+    private List<String> lines = new ArrayList<>();
+
+    public TrackLogConverter(String fileName, String inDir, String outDir) {
+        this.fileName = fileName;
+        this.inDir = inDir;
+        this.outDir = outDir;
+    }
+
+    @Override
+    public void run() {
+        try {
+            readFile();
+            removeHeaderFailLines();
+            removeLogWhenStop();
+            replaceInvalid();
+            replaceMonth();
+            writeFile();
+        } catch (IOException e) {
+            System.out.printf("IO error in file: %s\n", this.fileName);
+        }
+    }
+
+    public static String monthToNumber(String month) {
+        return switch (month) {
+            case "янв." -> "01";
+            case "февр." -> "02";
+            case "мар." -> "03";
+            case "апр." -> "04";
+            case "мая" -> "05";
+            case "июн." -> "06";
+            case "июл." -> "07";
+            case "авг." -> "08";
+            case "сент." -> "09";
+            case "окт." -> "10";
+            case "нояб." -> "11";
+            case "дек." -> "12";
+            default -> throw new IllegalStateException("Unexpected value: " + month);
+        };
+    }
+
+    private void readFile() throws IOException {
+        String fileName = this.inDir + "/" + this.fileName;
+        try (BufferedReader fileReader = new BufferedReader(new FileReader(fileName))) {
+            if (fileReader.ready()) {
+                this.header = fileReader.readLine();
+            }
+            while (fileReader.ready()) {
+                this.lines.add(fileReader.readLine());
+            }
+        }
+    }
+
+    private void removeHeaderFailLines() {
+        this.lines.removeIf(s -> s.chars().filter(c -> c == '-').count() > 4);
+    }
+
+    private boolean isNormalVoltage(String line) {
+        String[] values = line.split(",");
+        return Double.parseDouble(values[values.length - 1]) > 12.0;
+    }
+
+    private void removeLogWhenStop() {
+        List<String> result = new ArrayList<>();
+
+        for (int i = 0; i < this.lines.size(); i++) {
+            if (((i < 2) || (i > this.lines.size() - 2))
+                    && isNormalVoltage(this.lines.get(i))) {
+                result.add(this.lines.get(i));
+            }
+            if ((i + 2 < this.lines.size()
+                    && isNormalVoltage(this.lines.get(i))
+                    && isNormalVoltage(this.lines.get(i + 1))
+                    && isNormalVoltage(this.lines.get(i + 2)))
+                    && (i - 2 >= 0
+                    && isNormalVoltage(this.lines.get(i))
+                    && isNormalVoltage(this.lines.get(i - 1))
+                    && isNormalVoltage(this.lines.get(i - 2)))) {
+                result.add(this.lines.get(i));
+            }
+        }
+        this.lines = result;
+    }
+
+    private void replaceInvalid() {
+        this.lines =  this.lines.stream()
+                .map(s -> s.replaceAll(",-,", ",0,"))
+                .collect(Collectors.toList());
+    }
+
+    private void replaceMonth() {
+        List<String> result = new ArrayList<>();
+        for (String line : this.lines) {
+            String month = line.split("-")[1];
+            String newMonth = monthToNumber(month);
+            result.add(line.replaceFirst(month, newMonth));
+        }
+        this.lines = result;
+    }
+
+    private String getOutFileName() {
+        String month = this.fileName.split("-")[2];
+        String outputFileName;
+        if (!month.matches("\\d{2}")) {
+            String monthNumber = monthToNumber(month);
+            outputFileName = fileName.replaceFirst(month, monthNumber);
+
+        } else {
+            outputFileName = this.fileName;
+        }
+        return outDir + "/" + outputFileName;
+    }
+
+    private void writeFile() throws IOException {
+        try (BufferedWriter fileWriter = new BufferedWriter(new FileWriter(getOutFileName()))) {
+            fileWriter.write(this.header + "\n");
+            for (String line : this.lines) {
+                fileWriter.write(line + "\n");
+            }
+        }
+    }
+}
